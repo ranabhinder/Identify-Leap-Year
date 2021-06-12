154c3f7c108133e4da2332a5de78c4b1ca99229a
ranabhinder1321 <ranabhinder@yahoo.co.nz>
2021-06-12 14:30:04 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Identify_Leap_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 0;
+            year = int.Parse(Console.ReadLine());
+
+            if ((year % 4==0) && (year% 100!=0) || (year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year ", year);
+            }
+
+            else
+            
+            {
+            Console.WriteLine("{0} is not a Leap Year", year);
+            }
+            Console.ReadKey();
+
+
+        }
+             
+    }
+}
